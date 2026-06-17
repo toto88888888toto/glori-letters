66d4b582dbd69d27a4d99d3c1cf9027570a119ef
Toto Outanavongsa <totooutanavongsa@Toto.local>
2026-06-17 16:46:52 +0700

    fix port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 80;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname)));
 
@@ -9,6 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
